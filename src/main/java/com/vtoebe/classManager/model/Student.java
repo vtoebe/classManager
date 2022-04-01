dd5f5b1a481d4a803fda515c0776fe1938ffdf2e
Vitória Toebe <vitoriatoebe@gmail.com>
2022-04-01 20:15:52 -0300

    refactor: add student constructor for studentRequest

--- src/main/java/com/vtoebe/classManager/model/Student.java
+++ src/main/java/com/vtoebe/classManager/model/Student.java
@@ -1,5 +1,6 @@
 package com.vtoebe.classManager.model;
 
+import com.vtoebe.classManager.request.StudentRequest;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,4 +15,12 @@ public class Student {
     private String className;
     private String email;
     private String password;
+
+    public Student(StudentRequest studentRequest){
+        this.id = UUID.randomUUID();
+        this.name = studentRequest.getName();
+        this.className = studentRequest.getClassName();
+        this.email = studentRequest.getEmail();
+        this.password = studentRequest.getPassword();
+    }
 }
--- src/main/java/com/vtoebe/classManager/service/StudentService.java
+++ src/main/java/com/vtoebe/classManager/service/StudentService.java
@@ -4,16 +4,9 @@ import com.vtoebe.classManager.model.Student;
 import com.vtoebe.classManager.request.StudentRequest;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
-
 @Service
 public class StudentService {
     public Student createStudent(StudentRequest studentRequest) {
-        return new Student(
-                UUID.randomUUID(),
-                studentRequest.getName(),
-                studentRequest.getClassName(),
-                studentRequest.getEmail(),
-                studentRequest.getPassword());
+        return new Student(studentRequest);
     }
 }
